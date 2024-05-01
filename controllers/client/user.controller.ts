d076352618a596b5a042ea46054fe8701fe19a5b
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-01 15:08:58 +0700

    login user

--- controllers/client/user.controller.ts
+++ controllers/client/user.controller.ts
@@ -2,14 +2,14 @@ import User from "../../models/user.model";
 import md5 from "md5";
 import { Request, Response } from "express";
 
-//[GET] /user/controller
+//[GET] /user/register
 export const register = (req: Request, res: Response) => {
   res.render("client/pages/user/register", {
     pageTitle: "Đăng ký tài khoản",
   });
 };
 
-//[POST] /user/controller
+//[POST] /user/register
 export const registerPost = async (req: Request, res: Response) => {
   const existEmail = await User.findOne({
     email: req.body.email,
@@ -24,5 +24,40 @@ export const registerPost = async (req: Request, res: Response) => {
   const user = new User(req.body);
   await user.save();
   //res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/user/login");
+};
+
+//[GET] /user/login
+export const login = (req: Request, res: Response) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+
+//[POST] /user/login
+export const loginPost = async (req: Request, res: Response) => {
+  const userAccount = await User.findOne({
+    email: req.body.email,
+    deleted: false,
+  });
+  if (!userAccount) {
+    req.flash("error", "Email không chính xác!");
+    res.redirect("back");
+    return;
+  }
+
+  if (userAccount.password !== md5(req.body.password)) {
+    req.flash("error", "Mật khẩu không chính xác!");
+    res.redirect("back");
+    return;
+  }
+
+  if (userAccount.status === "inactive") {
+    req.flash("error", "Tài khoản của bạn đang bị khóa!");
+    res.redirect("back");
+    return;
+  }
+
+  res.cookie("tokenUser", userAccount.tokenUser);
   res.redirect("/");
 };
--- validates/client/user.validate.ts
+++ validates/client/user.validate.ts
@@ -24,3 +24,22 @@ export const registerPost = async (
   }
   next();
 };
+
+export const loginPost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.body.email) {
+    req.flash("error", "Email không được để trống");
+    res.redirect("back");
+    return;
+  }
+
+  if (!req.body.password) {
+    req.flash("error", "Mật khẩu không được để trống");
+    res.redirect("back");
+    return;
+  }
+  next();
+};
