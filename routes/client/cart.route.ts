bc435f43d289ac5172a95db7a4e54fcbdf6c45c6
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-01 14:13:56 +0700

    trang giỏ hàng

--- routes/client/cart.route.ts
+++ routes/client/cart.route.ts
@@ -3,6 +3,7 @@ const router: Router = Router();
 
 import * as controller from "../../controllers/client/cart.controller";
 
+router.get("/", controller.index);
 router.post("/add/:productId", controller.addPost);
 
 export const cartRoutes: Router = router;
