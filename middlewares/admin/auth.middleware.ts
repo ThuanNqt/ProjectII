a134927b5bb7280d6178ae5a138259de1c016c52
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-15 14:18:50 +0700

    Phân quyền trên giao diện

--- middlewares/admin/auth.middleware.ts
+++ middlewares/admin/auth.middleware.ts
@@ -1,5 +1,27 @@
 import Account from "../../models/account.model";
+import Role from "../../models/role.model";
 import { Request, Response, NextFunction } from "express";
+
+interface IAccount {
+  fullName: string,
+    password: string,
+    email: string,
+    token: string,
+    phone: string,
+    avatar: string,
+    role_id: string,
+    status: string,
+    deleted: boolean,
+    deletedAt: Date,
+}
+
+interface IRole{
+  title: string,
+    description: string,
+    permissions: string[],
+    deleted: boolean,
+    deletedAt: Date,
+}
 
 export const requireAuth = async (
   req: Request,
@@ -10,16 +32,20 @@ export const requireAuth = async (
     res.redirect(`/admin/auth/login`);
     return;
   } else {
-    const user = await Account.findOne({
+    const user:IAccount = await Account.findOne({
       token: req.cookies.token,
       deleted: false,
-    });
+    }).select("-password");
     if (!user) {
       res.redirect(`/admin/auth/login`);
       return;
     } else {
+      const role:IRole = await Role.findOne({ _id: user.role_id }).select(
+        "title permissions"
+      );
       // Khai báo app locals để tất cả file pug đều có thể sử dụng được như biến prefixAdmin
       res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
